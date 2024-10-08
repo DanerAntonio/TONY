10d942d7db6e10a4a1190d47f8018e0be5f14b32
CristoteAma <danerbedoya@gmail.com>
2024-10-07 21:46:06 -0500

    actualizacion

--- models/Compra.js
+++ models/Compra.js
@@ -1,12 +1,28 @@
 // models/Compra.js
 const mongoose = require('mongoose');
-const Producto = require('./Producto');
+
+const detalleCompraSchema = new mongoose.Schema({
+  producto: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto', required: true },
+  cantidad: { type: Number, required: true },
+  precioUnitario: { type: Number, required: true }
+});
 
 const compraSchema = new mongoose.Schema({
+  idCompra: { type: Number, unique: true },
   fecha: { type: Date, default: Date.now },
   proveedor: { type: mongoose.Schema.Types.ObjectId, ref: 'Proveedor', required: true },
+  detalles: [detalleCompraSchema],
   total: { type: Number, required: true },
   estado: { type: String, enum: ['Activa', 'Anulada'], default: 'Activa' }
+});
+
+// Middleware para auto-incrementar idCompra
+compraSchema.pre('save', async function(next) {
+  if (!this.idCompra) {
+    const lastCompra = await this.constructor.findOne({}, {}, { sort: { 'idCompra': -1 } });
+    this.idCompra = lastCompra && lastCompra.idCompra ? lastCompra.idCompra + 1 : 1;
+  }
+  next();
 });
 
 module.exports = mongoose.model('Compra', compraSchema);
